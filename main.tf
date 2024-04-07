f8bb0421c7caaa892d65b6bce9ece15c587a1558
Namburu <vasu.namburu@capgemini.com>
2024-04-07 17:36:40 +0530

    formatting tf file

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
-resource "google_storage_bucket" "my-bucket" {
-  name                     = "gcp-tfm-my-bucket-vasu2511"
+resource "google_storage_bucket" "bucket" {
+  name                     = "gcp-tfm-my-bucket-vasu1923"
   project                  = "vasu-first-project"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
+}
+
+resource "google_storage_bucket_object" "default" {
+ name         = "abc.txt"
+ source       = "~/abc.txt"
+ content_type = "text/plain"
+ bucket       = google_storage_bucket.bucket.id
 }
