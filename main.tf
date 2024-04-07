f829464f08ca1cd2d097d46d583ac62860583d76
Namburu <vasu.namburu@capgemini.com>
2024-04-07 19:34:07 +0530

    dataproc cluster start/create

--- main.tf
+++ main.tf
@@ -4,23 +4,9 @@ provider "google" {
 }
 
 
-# Define the existing Dataproc cluster
-resource "google_dataproc_cluster" "example_cluster" {
-  name   = "my-first-cluster"
-  region = "us-central1"
-
-  # Omitted other cluster configuration options for brevity
-
-  # Use lifecycle to prevent changes to the cluster's configuration
-  lifecycle {
-    ignore_changes = [config]
-  }
-}
-
 # Start the existing Dataproc cluster
 resource "null_resource" "start_cluster" {
-  depends_on = [google_dataproc_cluster.example_cluster]
-
+  count = 0
   # Use a local-exec provisioner to execute gcloud command to start the cluster
   provisioner "local-exec" {
     command = "gcloud dataproc clusters start my-first-cluster --region your-region --project vasu-first-project"
