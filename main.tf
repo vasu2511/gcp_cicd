2c18dae8cfd615e82b1920d1e13a3c134f7db4f0
Namburu <vasu.namburu@capgemini.com>
2024-04-07 19:28:47 +0530

    dataproc cluster start/create

--- main.tf
+++ main.tf
@@ -3,14 +3,6 @@ provider "google" {
   region  = "us-central1"
 }
 
-# Start an existing Dataproc cluster
-resource "google_dataproc_cluster" "existing_cluster" {
-  name   = "my-first-cluster"
-  region = "us-central1"
-
-  # Omitted other cluster configuration options since it's an existing cluster
-}
-
 resource "google_storage_bucket" "bucket" {
   name                     = "gcp-tfm-my-bucket-vasu1923"
   location                 = "US"
