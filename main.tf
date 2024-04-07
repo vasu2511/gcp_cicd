4150d1eb9ee17eb10856ea5705e36ce50dff1715
Namburu <vasu.namburu@capgemini.com>
2024-04-07 17:43:11 +0530

    copying file to gcs

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "bucket" {
-  name                     = "gcp-tfm-my-bucket-vasu1923"
+  name                     = "gcp-tfm-my-bucket-vasu1929"
   project                  = "vasu-first-project"
   location                 = "US"
   force_destroy            = true
@@ -8,7 +8,7 @@ resource "google_storage_bucket" "bucket" {
 
 resource "google_storage_bucket_object" "default" {
   name         = "abc.txt"
-  source       = "~/abc.txt"
+  source       = "/abc.txt"
   content_type = "text/plain"
   bucket       = google_storage_bucket.bucket.id
 }
