308a549e98e4d9b175b6001183f0e2a8b2d3a712
Namburu <vasu.namburu@capgemini.com>
2024-04-07 19:24:01 +0530

    dataproc cluster start/create

--- main.tf
+++ main.tf
@@ -3,45 +3,12 @@ provider "google" {
   region  = "us-central1"
 }
 
-
-# Check if the cluster already exists
-data "google_dataproc_cluster" "existing_cluster" {
+# Start an existing Dataproc cluster
+resource "google_dataproc_cluster" "existing_cluster" {
   name   = "my-first-cluster"
   region = "us-central1"
-}
 
-# Create or start the cluster based on existence
-resource "google_dataproc_cluster" "example_cluster" {
-  count  = length(data.google_dataproc_cluster.existing_cluster)
-  name   = "my-first-cluster"
-  region = "us-central1"
-  # Add other cluster configuration options as needed
-
-  # Only create the cluster if it doesn't exist
-  lifecycle {
-    ignore_changes = [name, config]
-  }
-}
-
-# Only start the existing cluster if it exists
-resource "google_dataproc_cluster" "start_cluster" {
-  count  = length(data.google_dataproc_cluster.existing_cluster)
-  name   = "my-first-cluster"
-  region = "us-central1"
-  # Add other cluster configuration options as needed
-
-  # Only start the cluster if it exists
-  lifecycle {
-    ignore_changes = [name, config]
-  }
-}
-
-# Run this block if the cluster doesn't exist
-resource "google_dataproc_cluster" "create_cluster" {
-  count  = var.create_cluster ? 1 : 0
-  name   = "cluster-terraform"
-  region = "us-central1"
-  # Add other cluster configuration options as needed
+  # Omitted other cluster configuration options since it's an existing cluster
 }
 
 resource "google_storage_bucket_object" "object" {
