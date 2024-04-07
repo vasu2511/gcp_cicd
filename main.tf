ca8071695b2e59525fd8150535b26979a382a8ed
Namburu <vasu.namburu@capgemini.com>
2024-04-07 17:25:56 +0530

    formatting tf file

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my-bucket" {
   name                     = "gcp-tfm-my-bucket-vasu2511"
-  project                  = "vasu-first-project-"
+  project                  = "vasu-first-project"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
