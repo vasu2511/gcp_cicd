7128cf513d59065711dbf2dd76fc729257fe6145
Namburu <vasu.namburu@capgemini.com>
2024-04-07 19:31:42 +0530

    dataproc cluster start/create

--- main.tf
+++ main.tf
@@ -3,11 +3,28 @@ provider "google" {
   region  = "us-central1"
 }
 
-resource "google_storage_bucket" "bucket" {
-  name                     = "gcp-tfm-my-bucket-vasu1923"
-  location                 = "US"
-  force_destroy            = true
-  public_access_prevention = "enforced"
+
+# Define the existing Dataproc cluster
+resource "google_dataproc_cluster" "example_cluster" {
+  name   = "my-first-cluster"
+  region = "us-central1"
+
+  # Omitted other cluster configuration options for brevity
+
+  # Use lifecycle to prevent changes to the cluster's configuration
+  lifecycle {
+    ignore_changes = [config]
+  }
+}
+
+# Start the existing Dataproc cluster
+resource "null_resource" "start_cluster" {
+  depends_on = [google_dataproc_cluster.example_cluster]
+
+  # Use a local-exec provisioner to execute gcloud command to start the cluster
+  provisioner "local-exec" {
+    command = "gcloud dataproc clusters start my-first-cluster --region your-region --project vasu-first-project"
+  }
 }
 
 resource "google_storage_bucket_object" "object" {
