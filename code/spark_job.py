13d6566cb5a4edb53bc2a41899cd35cd878d9c0f
Namburu <vasu.namburu@capgemini.com>
2024-04-09 13:18:51 +0530

    deploying pyspark job

---
+++ code/spark_job.py
@@ -0,0 +1,12 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import lit
+
+spark = SparkSession.builder.getOrCreate()
+
+df1 = spark.read.option("header","true").csv("gs://my-bucket-vasu2511/StudentData.csv")
+
+df2 = df1.withColumn("extra_column",lit("extra_data"))
+
+df2.write.option("header","true").mode("overwrite").csv("gs://my-bucket-vasu2511/output/")
+
+print("completed")
