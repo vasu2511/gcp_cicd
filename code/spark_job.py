97250680874ee8c969f51d6ba3738ba6c04f8e5f
Namburu <vasu.namburu@capgemini.com>
2024-04-12 19:56:34 +0530

    code changes

--- code/spark_job.py
+++ code/spark_job.py
@@ -5,7 +5,7 @@ spark = SparkSession.builder.getOrCreate()
 
 df1 = spark.read.option("header","true").csv("gs://my-bucket-vasu2511/input/StudentData.csv")
 
-df2 = df1.withColumn("extra_column",lit("extra_data"))
+df2 = df1.withColumn("vasu_extra_column",lit("extra_data"))
 
 df2.write.option("header","true").mode("overwrite").csv("gs://my-bucket-vasu2511/output/")
 
